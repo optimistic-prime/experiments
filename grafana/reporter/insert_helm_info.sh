37487565caac039274483ad4bffae3e54218cb43
Alex Romine <alex.romine@unsupervised.com>
2020-06-11 10:07:59 -0500

    good progress on reporter and stuff

---
+++ grafana/reporter/insert_helm_info.sh
@@ -0,0 +1,17 @@
+USER='postgresadmin'
+PASS='admin123'
+HOST='postgres.default.svc.cluster.local'
+DB='helm_info'
+
+TABLE='helm_schema.helm_table'
+
+helm ls -o json | jq -c '.[]' | grep unsupervised | while read -r line
+do
+  NAME=$(echo $line | jq -r .name)
+  STATUS=$(echo $line | jq -r .status)
+  APP_VERSION=$(echo $line | jq -r .app_version)
+  LAST_UPDATE=$(echo $line | jq -r .updated | cut -d "." -f 1)
+
+  PGPASSWORD=$PASS psql -U $USER -h $HOST -d $DB -p 5432 -c "INSERT INTO helm_schema.helm_table (name, status, app_version, last_update) VALUES ('$NAME', '$STATUS', '$APP_VERSION', '$LAST_UPDATE');"
+done
+
